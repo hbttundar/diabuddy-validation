a56ea4fec2d794c1201f5ea0a2efed87fc7b68d1
hbagherzadegan <hossein.bagherzadegan@kollex.de>
2025-04-04 22:11:16 +0200

    - fix test issues

--- field_validator_test.go
+++ field_validator_test.go
@@ -13,7 +13,7 @@ func TestFieldValidatorWithOneRule(t *testing.T) {
 	fv := v.ForField("email", "")
 	assert.NotNil(t, fv, "expected FieldValidator but got nil")
 
-	fv.AddRule(rules.EmailRule{})
+	fv.AddRule(rules.EmailRule{FieldName: "email"})
 	assert.NotEmpty(t, v.fields["email"], "expected rules to be added for the email field")
 }
 
@@ -23,8 +23,8 @@ func TestFieldValidatorWithTwoRule(t *testing.T) {
 	fv := v.ForField("id", nil)
 	assert.NotNil(t, fv, "expected FieldValidator but got nil")
 
-	fv.AddRule(rules.UuidRule{})
-	fv.AddRule(rules.RequiredRule{})
+	fv.AddRule(rules.UuidRule{FieldName: "id"})
+	fv.AddRule(rules.RequiredRule{FieldName: "id"})
 	assert.NotEmpty(t, v.fields["id"], "expected rules to be added for the id field")
 	assert.Len(t, v.fields["id"], 2, "expected 2 rules for the 'id' field")
 }
